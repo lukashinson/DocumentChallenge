c4bb9f56cef2a2d67ce0e7ae633236edec602ce1
lukas hinson <lrhdh6@mail.missouri.edu>
2019-05-10 19:14:55 -0500

    code

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Document
+{
+
+    class Document
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("Document");
+
+                Console.WriteLine("Enter Document Title:");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter Document Content:");
+
+                string content = Console.ReadLine();
+
+                string filename = name + ".txt";
+
+                string path = Environment.CurrentDirectory + "/" + filename;
+
+                if (!File.Exists(path))
+
+                {
+
+                    File.WriteAllText(path, content);
+
+                }
+
+                Console.WriteLine(filename + " was successfully saved. The document contains " + content.Length + " characters");
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e);
+
+            }
+
+        }
+
+    }
+
+}
